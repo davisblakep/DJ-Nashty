63682137484f4f4af55f4ea8d4733ee58b074eb4
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-02-14 22:34:35 -0500

    Saving Initial Commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+gsap.from('img', {
+    opacity: 0,
+    x: 200,
+    duration: 1, 
+})
+
+gsap.from('h2', {
+    opacity: 0,
+    x: -200,
+    duration: 1,
+    stagger: 0.25,
+    
+})
+
+gsap.from('p', {
+    opacity: 0,
+    x: -200,
+    duration: 1,
+    stagger: 0.25,
+})
+
+gsap.from('header', {
+    opacity: 0,
+    y: -200,
+    duration: 1,
+})
+
+gsap.from('.icons', {
+    opacity: 0,
+    y: 200,
+    duration: 1,
+})
+
+gsap.from('.topMainPhoto', {
+    opacity: 0,
+    x: 200,
+    duration: 1, 
+})
